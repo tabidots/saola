7acb7a81593d3ad898d672d99b72a5184820e386
Justin Douglas <tabidots@gmail.com>
2025-12-08 18:23:32 +0700

    Revert audio wordlist to old version (was updated by mistake)

--- web/src/js/data-loader.js
+++ web/src/js/data-loader.js
@@ -7,7 +7,7 @@ export let enIndex = null;
 export let glossDocIndex = null;
 export let vnHeadwordSet = null;
 
-const DATA_VERSION = 'v4';
+const DATA_VERSION = 'v5';
 
 export async function initializeData() {
     // Load data
