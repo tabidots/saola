002ed0dbd9a8e44c83bdd1ff614173ebb17283c7
Justin Douglas <tabidots@gmail.com>
2025-12-19 13:42:05 +0700

    Remove "dzậy"
    dzậy was being incorrectly swapped with dậy as an alt spelling in the source DB

--- web/src/js/data-loader.js
+++ web/src/js/data-loader.js
@@ -7,7 +7,7 @@ export let enIndex = null;
 export let glossDocIndex = null;
 export let vnHeadwordSet = null;
 
-const DATA_VERSION = 'v9';
+const DATA_VERSION = 'v10';
 
 export async function initializeData() {
     // Load data
